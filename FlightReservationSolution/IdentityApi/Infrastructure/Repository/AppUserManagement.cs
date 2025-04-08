90cc8411e3b5786538bc5a0a9a8f6ad074accb8e
fabianoGDB <doctorfabi.f@gmail.com>
2025-04-07 23:47:32 -0300

    addiing repositories classes

--- FlightReservationSolution/IdentityApi/Infrastructure/Repository/AppUserManagement.cs
+++ FlightReservationSolution/IdentityApi/Infrastructure/Repository/AppUserManagement.cs
@@ -1,7 +1,27 @@
-﻿namespace IdentityApi.Infrastructure.Repository
-{
-    public class AppUserManagement()
-    {
+﻿using Grpc.Core;
+using IdentityApi.Domain;
+using Microsoft.AspNetCore.Identity;
 
+namespace IdentityApi.Infrastructure.Repository
+{
+    public class AppUserManagement(UserManager<AppUser> userManger) : IAppUser
+    {
+        public async Task<bool> CreateAsync(AppUser user)
+        {
+            var result = await userManger.CreateAsync(user, user.PasswordHash!);
+            if (result.Succeeded) { return true; }
+            string error = string.Join("; ", result.Errors.Select(e => e.Description));
+            throw new RpcException(new Status(StatusCode.FailedPrecondition, error));
+        }
+
+        public async Task<AppUser?> GetByEmail(string email) => await userManger.FindByEmailAsync(email);
+
+        public Task<AppUser?> GetById(string userId) => userManger.FindByIdAsync(userId);
+
+        public async Task<bool> PasswordMatchAsync(AppUser user, string plainPassword)
+        {
+            bool result = await userManger.CheckPasswordAsync(user, plainPassword);
+            return result;
+        }
     }
 }
---
+++ FlightReservationSolution/IdentityApi/Infrastructure/Repository/JwtTokenManagement.cs
@@ -0,0 +1,25 @@
+﻿using IdentityApi.Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityApi.Infrastructure.Repository
+{
+    public class JwtTokenManagement(IConfiguration configuration) : IJwtToken
+    {
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var securityToken = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityToken, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
